0a113217f191012715b1396f1c7e4bccfa7da15d
Cory Clark <corclark@gmail.com>
2018-07-18 13:04:27 -0600

    Cleanup

--- app/src/main/java/com/cirrusmd/cirrusmdexample/ExampleApp.java
+++ app/src/main/java/com/cirrusmd/cirrusmdexample/ExampleApp.java
@@ -2,11 +2,6 @@ package com.cirrusmd.cirrusmdexample;
 
 import android.app.Application;
 
-import com.google.gson.GsonBuilder;
-
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 /**
  * Created by Cory Clark on 12/8/17
  */
@@ -15,7 +10,5 @@ public class ExampleApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-
     }
 }
